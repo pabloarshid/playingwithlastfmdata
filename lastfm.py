f2c4254e9d16497d6b7f9343f282bf060126c544
PabloArshid <umarsa.birvancity@gmail.com>
2018-11-04 00:45:51 -0700

    first plot

--- lastfm.py
+++ lastfm.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+
 headers = ["Artist", "Album", "Song", "DateTime"]
 lastfm = pd.read_csv('umarsabir.csv', names = headers, parse_dates=[3])
+
+
 def get_season(row):
     if row['Date'].month >= 3 and row['Date'].month <= 5:
         return '1'
@@ -11,7 +14,24 @@ def get_season(row):
         return '3'
     else:
         return '4'
+
+
 temp = pd.DatetimeIndex(lastfm['DateTime'])
 lastfm['Date'] = temp.date
 lastfm['Time'] = temp.time
-del lastfm['DateTime']
+lastfm['Date'] = pd.to_datetime(lastfm['Date'])
+DotW = lastfm['Date'].dt.weekday_name
+lastfm['Day of the Week'] = DotW
+lastfm['Season'] = lastfm.apply(get_season, axis=1)
+lastfm = lastfm.dropna()
+lastfm = lastfm[lastfm.Date.dt.year >= 2010]
+
+columns = ['Month', 'count of plays in month', 'day', 'plays on date']
+
+time = pd.DataFrame(index=['day'], columns=columns)
+time1 = lastfm['Date'].dt.month
+
+time  = pd.DataFrame(time1.value_counts())
+
+time.sort_index(inplace=True)
+plt.plot(time.index, time.Date)
